5b8e236d7d36aa3768ff7248dd43b8f8bafb7aa1
DaizoMori39 <134017757+DaizoMori39@users.noreply.github.com>
2023-06-03 19:17:07 -0400

    Update reaplaceCards function script.js

--- script.js
+++ script.js
@@ -203,5 +203,7 @@ function replaceCards(deck, selectedCards) {
     for (let i = 0; i < selectedCards.length; i++) {
       const randomCard = Array.from(deck)[Math.floor(Math.random() * deck.size)];
       selectedCards[i] = randomCard;
+      deck.delete(randomCard);
+  }
     }
 }
